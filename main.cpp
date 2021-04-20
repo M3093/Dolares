3f182bb0ac2e35ae6f8b45ad3b43e3ebb7fddb08
Maria Badilla <>
2021-04-20 13:43:30 +0000

    Cambio

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@ using namespace std;
 float dolares, colones, conversion; 
 int main() {
  cout<<"Ingrese el tipo de cambio del dolar "<<endl;
-    cin>>dolares;
-    cout<<"Ingrese la cantidad de dolares"<<endl;
     cin>>colones;
+    cout<<"Ingrese la cantidad de dolares"<<endl;
+    cin>>dolares;
     conversion=(dolares*colones);
     cout<<"El monto de dolares a colones es  :"<<conversion<<endl;
     return 0;
